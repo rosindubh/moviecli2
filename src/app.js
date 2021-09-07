fe860558171ce5dec2c0c28d47b0e492f11994da
rosindubh <p.welsby2@ntlworld.com>
2021-09-07 20:07:33 +0100

    create a function to read storage.json / function app in app.js - tested both ok

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 //phil welsby - 7 sept 2021
 //app.js
+
+const request = process.argv[2];
+
+const app = () => {
+    console.log('Hello from app function')
+}
+
+app();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,17 @@
 //phil welsby - 7 sept 2021
 //index.js
+
+const fs = require("fs");
+
+const readFile = () => {
+    try {
+        console.log("try in readFile")
+        return JSON.parse(fs.readFileSync("./src/db/storage.json"));
+    } catch (error) {
+        return [];
+    }
+}
+
+readFile()
+
+
